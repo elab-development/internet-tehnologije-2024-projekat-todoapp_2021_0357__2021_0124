c2ba044be9c77a22c76fae473f912f1c85b17552
kilometar <skolujemse@gmail.com>
2025-09-25 21:18:31 +0200

    napravljene veze izmedju modela

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +45,19 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    //povezivanje sa beleškama
+    
+    public function notes(): HasMany
+    {
+        return $this->hasMany(Note::class);
+    }
+
+    //povezivanje sa zadacima
+    
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
     }
 }
