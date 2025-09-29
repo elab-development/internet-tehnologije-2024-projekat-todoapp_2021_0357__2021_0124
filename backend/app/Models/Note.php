4317adef943800e92a8baa6a1f32c5283d90a1b6
anamaksimovic11 <am20210357@student.fon.bg.ac.rs>
2025-09-29 16:22:27 +0200

    Povezivanje tagova i beleški

--- backend/app/Models/Note.php
+++ backend/app/Models/Note.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Note extends Model
 {
@@ -26,5 +27,12 @@ class Note extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    //veza sa tagovima
+    
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
     }
 }
--- backend/app/Models/Tag.php
+++ backend/app/Models/Tag.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
@@ -12,4 +13,11 @@ class Tag extends Model
     protected $fillable = [
         'name',
     ];
+
+    //veza sa beleškama
+    
+    public function notes(): BelongsToMany
+    {
+        return $this->belongsToMany(Note::class);
+    }
 }
